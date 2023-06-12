58de8caa4514bcc1fa0a8e06813dd507e4393a0d
kmin <pretty981210@gmail.com>
2023-06-12 10:00:16 +0900

    Feat: Article CRUD/ 일반회원가입 로그인 추가
    - 게시글 리스트/작성/수정/삭제/상세보기
    - 일반회원 회원가입/이메일 인증/로그인
    
    related_to #8 #9

--- article/models.py
+++ article/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
-# Create your models here.
+from user.models import User
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    article_image = models.ImageField(upload_to='article/%Y/%m', blank=True)
+    song = models.FileField(upload_to='songs/', blank=True)
+    good = models.ManyToManyField(User, related_name='good_article',blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #Tags = models.ManytoManyField(Tag, related_name='articles',blank=True)
+    #Genre = models.ManytoManyField(Genre, related_name='articles',blank=True)
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #good = models.ManyToManyField(User, related_name='good_comment',blank=True)
